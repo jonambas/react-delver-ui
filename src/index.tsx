30afb98674cbc2f2155075d39bad11d421a3fcac
jonambas <jon@jonambas.com>
2022-07-01 18:38:00 -0400

    feat: document title and favicon

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { App } from './App';
 import { renderRoot } from './utils/root';
 
+document.title = __delverConfig.title;
 const out = document.createElement('div');
 document.body.append(out);
-
 renderRoot(<App />, out);
