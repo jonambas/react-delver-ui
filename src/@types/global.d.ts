62d0d8bc61955cdd42b21d574342115042e8c37d
jonambas <jon@jonambas.com>
2022-07-01 18:30:10 -0400

    feat: remove window config

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -3,8 +3,5 @@ export {};
 declare global {
   type Config = import('../../types').Config;
   type Internal = import('../../types').InternalConfig;
-  interface Window {
-    __delverConfig: Config & Internal;
-  }
   export const __delverConfig: Config & Internal;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { App } from './App';
 import { renderRoot } from './utils/root';
 
-window.__delverConfig = __delverConfig;
 const out = document.createElement('div');
 document.body.append(out);
 
